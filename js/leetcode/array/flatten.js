fd9c2fa5a48aa524807bd6f615e41f45a64ba94a
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-08 11:00:53 +0500

    Update prettier settings and flattened array solution

--- js/leetcode/array/flatten.js
+++ js/leetcode/array/flatten.js
@@ -1,12 +1,36 @@
 
-
 var flat = function (arr, n) {
+  if(n === 0) return arr;
+
   const result = [];
 
+  const helperFlat = (currArr, currDepth) => {
+    if(currDepth === n) return currArr;
 
+    const newResult = [];
+
+    for(let j = 0; j< currArr.length; j++){
+      if(Array.isArray(currArr[j])){
+        newResult.push(...helperFlat(currArr[j], currDepth + 1 ))
+      } else {
+        newResult.push(currArr[j]);
+      }
+    }
+
+    return newResult;
+  };
+
+  for(let i = 0; i< arr.length; i++) {
+    if(Array.isArray(arr[i])) {
+      result.push(...helperFlat(arr[i], 1)) ;
+    }else {
+      result.push(arr[i])
+    }
+  }
+
+  return result
 };
 
-
-console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 1));
+console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11, [2, [23,43,5]]], 12], [13, 14, 15]], 4));
 
 
