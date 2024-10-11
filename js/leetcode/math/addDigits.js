50bbbd09fdc775b694369153a4283e9821d54315
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-11 11:48:50 +0500

    feat(uglyNumber): third solution added

--- js/leetcode/math/addDigits.js
+++ js/leetcode/math/addDigits.js
@@ -1,23 +1,19 @@
 var addDigits = function (num) {
   if (num < 10) return num;
-  let increment = 1;
+  let ss = num;
 
-  while (String(num).length !== 1) {
+  while (String(ss).length !== 1) {
     let sum = 0;
-    let ss = num;
 
     while (ss !== 0) {
       sum = sum + Math.floor(ss % 10);
-      ss /= 10;
-
-      console.log('increment =>', increment);
-      increment++;
+      ss = Math.floor(ss / 10);
     }
 
-    num = sum;
+    ss = sum;
   }
 
-  return num;
+  return ss;
 };
 
 console.log(addDigits(16));
