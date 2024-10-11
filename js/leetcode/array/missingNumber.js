8160ab5eafa36961b2810aefc8fa0124fa39404f
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-11 15:33:41 +0500

    feat(missingNumber): second solution provided

--- js/leetcode/array/missingNumber.js
+++ js/leetcode/array/missingNumber.js
@@ -1,9 +1,13 @@
 var missingNumber = function (nums) {
-  const map = new Set(nums);
-
   for (let i = 0; i <= nums.length; i++) {
-    if (!map.has(i)) return i;
+    if (!nums.includes(i)) return i;
   }
+
+  // const map = new Set(nums);
+  //
+  // for (let i = 0; i <= nums.length; i++) {
+  //   if (!map.has(i)) return i;
+  // }
 };
 
 console.log(missingNumber([0, 2, 3]));
