e3aba1892deee8bc4d1c1e0565cb0309abfc56d8
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-11 15:47:30 +0500

    feat(missingNumber): fourth solution provided

--- js/leetcode/array/missingNumber.js
+++ js/leetcode/array/missingNumber.js
@@ -2,14 +2,19 @@ var missingNumber = function (nums) {
   // for (let i = 0; i <= nums.length; i++) {
   //   if (!nums.includes(i)) return i;
   // }
-  for (let i = 0; i <= nums.length; i++) {
-    if (nums.indexOf(i) < 0) return i;
-  }
+  // for (let i = 0; i <= nums.length; i++) {
+  //   if (nums.indexOf(i) < 0) return i;
+  // }
   // const map = new Set(nums);
   //
   // for (let i = 0; i <= nums.length; i++) {
   //   if (!map.has(i)) return i;
   // }
+
+  const sum = (nums.length * (nums.length + 1)) / 2;
+  const sumExistance = nums.reduce((prev, item) => prev + item, 0);
+
+  return sum - sumExistance;
 };
 
 console.log(missingNumber([0, 2, 1, 3]));
