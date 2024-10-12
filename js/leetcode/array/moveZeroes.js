2387098d37e42b6d91112cb7b3cdb70412f0f950
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-12 16:21:27 +0500

    feat(moveZeros): second solution provided

--- js/leetcode/array/moveZeroes.js
+++ js/leetcode/array/moveZeroes.js
@@ -1,19 +1,35 @@
+// 1 - solution
+
+// var moveZeroes = function (nums) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i; j < nums.length; j++) {
+//       if (nums[i] === 0 && nums[j] !== 0) {
+//         const temp = nums[j];
+//         nums[j] = nums[i];
+//         nums[i] = temp;
+//       }
+//     }
+//   }
+//
+//   return nums;
+// };
+
+// 2 - solution
+
 var moveZeroes = function (nums) {
+  const zerosArray = [];
+  const numbersArray = [];
+
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      if (nums[i] === 0 && nums[j] !== 0) {
-        const temp = nums[j];
-        nums[j] = nums[i];
-        nums[i] = temp;
-      }
-    }
+    if (nums[i] === 0) zerosArray.push(0);
+    else numbersArray.push(nums[i]);
   }
 
-  return nums;
+  return [...numbersArray, ...zerosArray];
 };
 
-// const nums = [0, 1, 0, 3, 12];
-const nums = [1, 2];
+const nums = [0, 1, 0, 3, 12];
+// const nums = [1, 2];
 
 // lastNonZeroIndex = 1 [1, 0, 0, 3, 12] i = 1;
 //
