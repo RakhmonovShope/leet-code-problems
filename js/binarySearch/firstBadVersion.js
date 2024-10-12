26ee9042a000f8854e5a222b1a4f405fd105e92d
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-12 14:36:33 +0500

    feat: second but very bad solution provided

--- js/binarySearch/firstBadVersion.js
+++ js/binarySearch/firstBadVersion.js
@@ -1,31 +1,43 @@
+// 1 - solution
+
+// var solution = function (isBadVersion) {
+//   const badVersions = new Set();
+//
+//   return function (n) {
+//     let pivot = Math.round(n / 2);
+//
+//     let max = n;
+//     let min = 0;
+//
+//     while (true) {
+//       if (isBadVersion(pivot)) {
+//         max = pivot;
+//
+//         badVersions.add(pivot);
+//
+//         pivot = Math.round((max + min) / 2);
+//       } else {
+//         min = pivot;
+//         pivot = Math.round((max + min) / 2);
+//       }
+//
+//       if (badVersions.has(pivot)) return pivot;
+//     }
+//   };
+// };
+
+// 2 - solution
+
 var solution = function (isBadVersion) {
-  const badVersions = new Set();
-
   return function (n) {
-    let pivot = Math.round(n / 2);
-
-    let max = n;
-    let min = 0;
-
-    while (true) {
-      if (isBadVersion(pivot)) {
-        max = pivot;
-
-        badVersions.add(pivot);
-
-        pivot = Math.round((max + min) / 2);
-      } else {
-        min = pivot;
-        pivot = Math.round((max + min) / 2);
-      }
-
-      if (badVersions.has(pivot)) return pivot;
+    for (let i = 0; i <= n; i++) {
+      if (isBadVersion(i)) return i;
     }
   };
 };
 
 const isBasVersion = (version) => {
-  return version > 3;
+  return version > 2;
 };
 
 console.log(solution(isBasVersion)(5));
