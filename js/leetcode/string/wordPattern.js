b19fafc8f46359703fbf0f3604d2bf4e0cd357f5
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-14 15:30:05 +0500

    feat: word pattern problem solved completely

--- js/leetcode/string/wordPattern.js
+++ js/leetcode/string/wordPattern.js
@@ -1,32 +1,30 @@
-var wordPattern = function (pattern, s) {
-  const words = s.split(' ');
+// 1 - solution provided
 
-  if (pattern.length !== words.length) return false;
+// var wordPattern = function (pattern, s) {
+//   const words = s.split(' ');
+//
+//   if (pattern.length !== words.length) return false;
+//
+//   const hash = new Map();
+//   const viseVersion = new Map();
+//
+//   for (let i = 0; i < pattern.length; i++) {
+//     if (hash.get(pattern[i]) || viseVersion.get(words[i])) {
+//       if (
+//         hash.get(pattern[i]) !== words[i] ||
+//         viseVersion.get(words[i]) !== pattern[i]
+//       )
+//         return false;
+//     } else {
+//       hash.set(pattern[i], words[i]);
+//       viseVersion.set(words[i], pattern[i]);
+//     }
+//   }
+//
+//   return true;
+// };
 
-  const hash = new Map();
-  const viseVersion = new Map();
-
-  for (let i = 0; i < pattern.length; i++) {
-    if (hash.has(pattern[i]) || viseVersion.has(words[i])) {
-      if (
-        hash.get(pattern[i]) !== words[i] ||
-        viseVersion.get(words[i]) !== pattern[i]
-      ) {
-        return false;
-      } else {
-        hash.delete(pattern[i]);
-        viseVersion.delete(words[i]);
-      }
-    } else {
-      hash.set(pattern[i], words[i]);
-      viseVersion.set(words[i], pattern[i]);
-    }
-  }
-
-  return !hash.keys.length || !viseVersion.keys.length;
-};
-
-const s = 'cat dog dog cat cat';
-const pattern = 'aaaa';
+const s = 'a a';
+const pattern = 'aa';
 
 console.log(wordPattern(pattern, s));
