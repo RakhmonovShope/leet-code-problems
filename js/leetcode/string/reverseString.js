1ffaf0bda0edb25081d7a082de4d43bbd062aa4d
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-15 11:18:32 +0500

    feat(reverseString): second solution provided

--- js/leetcode/string/reverseString.js
+++ js/leetcode/string/reverseString.js
@@ -1,11 +1,17 @@
 // 1 - solution
+//
+// var reverseString = function (s) {
+//   for (let i = 0; i < Math.floor(s.length / 2); i++) {
+//     [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
+//   }
+//
+//   return s;
+// };
+
+// 2 - solution
 
 var reverseString = function (s) {
-  for (let i = 0; i < Math.floor(s.length / 2); i++) {
-    [s[i], s[s.length - i - 1]] = [s[s.length - i - 1], s[i]];
-  }
-
-  return s;
+  return s.reverse();
 };
 
 const stringArr = ['h', 'e', 'l', 'l', 'o', 'd'];
