9758bd563cb85da4acb2c0f982e549abf1ebeb00
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-18 11:47:32 +0500

    feat(numberOf1Bits): second solution provided

--- js/leetcode/bitManipulation/numberOf1Bits.js
+++ js/leetcode/bitManipulation/numberOf1Bits.js
@@ -1,20 +1,32 @@
+// 1 - solution
+// const numberOf1Bits = (n) => {
+//   const binaryString = n.toString(2);
+//   let count = 0;
+//   let i = 0;
+//
+//   while (i < binaryString.length) {
+//     if (binaryString[i] === '1') count++;
+//
+//     i++;
+//   }
+//
+//   return count;
+// };
+
+// 2 - solution
+
 const numberOf1Bits = (n) => {
-  const binaryString = n.toString(2);
   let count = 0;
-  let i = 0;
 
-  while (i < binaryString.length) {
-    if (binaryString[i] === '1') count++;
+  while (n) {
+    count += n & 1;
 
-    i++;
+    n = n >> 1;
   }
 
   return count;
 };
 
-const t = 128;
+const t = 11;
 
 console.log(numberOf1Bits(t));
-
-const a = 0b11;
-console.log('a =>', a);
