ba0e362c45ffc18a9fc0d4c20d60907850c6f4af
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-19 15:04:38 +0500

    feat(reverseVowels): third solution provided

--- js/leetcode/string/reverseWovels.js
+++ js/leetcode/string/reverseWovels.js
@@ -25,51 +25,80 @@
 
 // 2 - solution
 
+// const reverseVowels = (s) => {
+//   const hash = new Set(['a', 'i', 'o', 'u', 'e']);
+//   const hashIndex = new Set();
+//
+//   const textArray = s.split('');
+//
+//   let firstIndex = 0;
+//   let lastIndex = s.length - 1;
+//
+//   for (let i = 0; i < textArray.length; i++) {
+//     if (
+//       hash.has(textArray[firstIndex].toLowerCase()) &&
+//       hash.has(textArray[lastIndex].toLowerCase()) &&
+//       !hashIndex.has(lastIndex)
+//     ) {
+//       const temp = textArray[firstIndex];
+//       textArray[firstIndex] = textArray[lastIndex];
+//       textArray[lastIndex] = temp;
+//       hashIndex.add(firstIndex);
+//       console.log('has two');
+//       firstIndex++;
+//       lastIndex--;
+//     } else if (
+//       !hash.has(textArray[firstIndex].toLowerCase()) &&
+//       !hash.has(textArray[lastIndex].toLowerCase())
+//     ) {
+//       console.log('no one');
+//       firstIndex++;
+//       lastIndex--;
+//     } else if (
+//       hash.has(textArray[firstIndex].toLowerCase()) &&
+//       !hash.has(textArray[lastIndex].toLowerCase())
+//     ) {
+//       console.log('no last');
+//       lastIndex--;
+//     } else if (
+//       hash.has(textArray[lastIndex].toLowerCase()) &&
+//       !hash.has(textArray[firstIndex].toLowerCase())
+//     ) {
+//       console.log('no first');
+//       firstIndex++;
+//     }
+//   }
+//
+//   return textArray.join('');
+// };
+
+// 3 - solution
+
 const reverseVowels = (s) => {
-  const hash = new Set(['a', 'i', 'o', 'u', 'e']);
-  const hashIndex = new Set();
+  const hash = new Set(['a', 'A', 'i', 'I', 'o', 'O', 'u', 'U', 'e', 'E']);
+  const textArr = s.split('');
 
-  const textArray = s.split('');
+  let lastIndex = textArr.length - 1;
 
-  let firstIndex = 0;
-  let lastIndex = s.length - 1;
+  for (let i = 0; i < textArr.length; i++) {
+    while (hash.has(textArr[i]) && !hash.has(textArr[lastIndex])) {
+      lastIndex--;
+    }
 
-  for (let i = 0; i < textArray.length; i++) {
     if (
-      hash.has(textArray[firstIndex].toLowerCase()) &&
-      hash.has(textArray[lastIndex].toLowerCase()) &&
-      !hashIndex.has(lastIndex)
+      hash.has(textArr[i]) &&
+      hash.has(textArr[lastIndex]) &&
+      i <= lastIndex
     ) {
-      const temp = textArray[firstIndex];
-      textArray[firstIndex] = textArray[lastIndex];
-      textArray[lastIndex] = temp;
-      hashIndex.add(firstIndex);
-      console.log('has two');
-      firstIndex++;
+      const temp = textArr[i];
+      textArr[i] = textArr[lastIndex];
+      textArr[lastIndex] = temp;
+
       lastIndex--;
-    } else if (
-      !hash.has(textArray[firstIndex].toLowerCase()) &&
-      !hash.has(textArray[lastIndex].toLowerCase())
-    ) {
-      console.log('no one');
-      firstIndex++;
-      lastIndex--;
-    } else if (
-      hash.has(textArray[firstIndex].toLowerCase()) &&
-      !hash.has(textArray[lastIndex].toLowerCase())
-    ) {
-      console.log('no last');
-      lastIndex--;
-    } else if (
-      hash.has(textArray[lastIndex].toLowerCase()) &&
-      !hash.has(textArray[firstIndex].toLowerCase())
-    ) {
-      console.log('no first');
-      firstIndex++;
     }
   }
 
-  return textArray.join('');
+  return textArr.join('');
 };
 
 // const text = 'IceCreAm';
@@ -78,5 +107,8 @@ const reverseVowels = (s) => {
 // const text = 'race a car';
 // const text = 'leetCode';
 const text = 'Egad! Loretta has Adams as mad as a hatter. Old age!';
+// const text = 'egad! Loretta has Adams as mad as a hatter. Old agE!';
+// const righ = 'egad! LOretta has adams as mad As a hatter. old agE!';
+// const righ = 'egad! LOretta has adams as mad As a hatter. old agE';
 // const text = 'egad! Loretta has Adams as mad as a hatter. Old agE!';
 console.log(reverseVowels(text));
