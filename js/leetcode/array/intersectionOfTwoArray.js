bbc19f5688a1d0472c89a53f25657b243d6f3034
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-19 16:10:16 +0500

    feat(intersectionOfTwoArray): second solution provided

--- js/leetcode/array/intersectionOfTwoArray.js
+++ js/leetcode/array/intersectionOfTwoArray.js
@@ -1,13 +1,29 @@
+// 1 - solution
+//
+// const intersectionOfTwoArray = (nums1, nums2) => {
+//   const concatenatedHash = new Set([...nums1, ...nums2]);
+//   const hash1 = new Set(nums1);
+//   const hash2 = new Set(nums2);
+//   const concatenatedArr = [...Array.from(concatenatedHash)];
+//   const result = [];
+//
+//   for (let i = 0; i < concatenatedArr.length; i++) {
+//     if (hash1.has(concatenatedArr[i]) && hash2.has(concatenatedArr[i])) {
+//       result.push(concatenatedArr[i]);
+//     }
+//   }
+//
+//   return result;
+// };
+
+// 2 - solution
+
 const intersectionOfTwoArray = (nums1, nums2) => {
-  const concatenatedHash = new Set([...nums1, ...nums2]);
-  const hash1 = new Set(nums1);
-  const hash2 = new Set(nums2);
-  const concatenatedArr = [...Array.from(concatenatedHash)];
   const result = [];
 
-  for (let i = 0; i < concatenatedArr.length; i++) {
-    if (hash1.has(concatenatedArr[i]) && hash2.has(concatenatedArr[i])) {
-      result.push(concatenatedArr[i]);
+  for (let i = 0; i < nums1.length; i++) {
+    if (!result.includes(nums1[i]) && nums2.includes(nums1[i])) {
+      result.push(nums1[i]);
     }
   }
 
