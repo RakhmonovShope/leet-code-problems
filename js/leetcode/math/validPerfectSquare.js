1b76a70d4ea359e2a083c32c13ac4e9263c86c86
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-20 23:09:06 +0500

    feat(validPerfectSquare): fourth solution provided

--- js/leetcode/math/validPerfectSquare.js
+++ js/leetcode/math/validPerfectSquare.js
@@ -1,11 +1,33 @@
-const validPerfectSquare = (num) => {
-  const isOdd = num % 2 !== 0;
+// 3 - solution
 
-  for (let i = isOdd ? 1 : 2; i <= Math.round(num / 2); i += 2) {
-    if (i * i === num) return true;
+// const validPerfectSquare = (num) => {
+//   const isOdd = num % 2 !== 0;
+//
+//   for (let i = isOdd ? 1 : 2; i <= Math.round(num / 2); i += 2) {
+//     if (i * i === num) return true;
+//   }
+//
+//   return false;
+// };
+
+// 4 - solution
+
+const validPerfectSquare = (num) => {
+  let mid = Math.round(num / 2),
+    min = 1,
+    max = num;
+
+  while (max >= min) {
+    mid = Math.floor((max + min) / 2);
+
+    if (Math.pow(mid, 2) === num) return true;
+
+    if (Math.pow(mid, 2) < num) {
+      min = mid + 1;
+    } else max = mid - 1;
   }
 
   return false;
 };
 
-console.log(validPerfectSquare(169));
+console.log(validPerfectSquare(16));
