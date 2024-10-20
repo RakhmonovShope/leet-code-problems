3ee3147241ac71808d3050e6e8018054a0dc7926
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-20 23:32:54 +0500

    feat(guessNumber): 2 - solution provided

--- js/leetcode/math/guessNumber.js
+++ js/leetcode/math/guessNumber.js
@@ -1,26 +1,36 @@
-var guess = function (num, target) {
-  if (num === target) {
-    return 0;
-  } else if (num > target) {
-    return -1;
-  } else if (num < target) return 1;
-};
+// 1- solution
+
+// var guess = function (num, target) {
+//   if (num === target) {
+//     return 0;
+//   } else if (num > target) {
+//     return -1;
+//   } else if (num < target) return 1;
+// };
+//
+// const guessNumber = (n) => {
+//   let mid = Math.round(n / 2),
+//     high = n,
+//     low = 0;
+//
+//   while (high >= low) {
+//     if (guess(mid) === 0) {
+//       return mid;
+//     } else if (guess(mid) === 1) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//
+//     mid = Math.floor((high + low) / 2);
+//   }
+// };
+
+// 2 - solution
 
 const guessNumber = (n) => {
-  let mid = Math.round(n / 2),
-    high = n,
-    low = 0;
-
-  while (high >= low) {
-    if (guess(mid) === 0) {
-      return mid;
-    } else if (guess(mid) === 1) {
-      low = mid + 1;
-    } else {
-      high = mid - 1;
-    }
-
-    mid = Math.floor((high + low) / 2);
+  for (let i = 0; i <= n; i++) {
+    if (guess(i) === 0) return i;
   }
 };
 
