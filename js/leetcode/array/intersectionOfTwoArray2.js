3517faf76a05301f9dc46906a8fa6f495e91faf4
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-21 22:40:55 +0500

    feat(intersectionOfTwo): second solution provided

--- js/leetcode/array/intersectionOfTwoArray2.js
+++ js/leetcode/array/intersectionOfTwoArray2.js
@@ -1,25 +1,48 @@
+// 1 - solution
+
+// const intersectionOfTwoArray2 = (nums1, nums2) => {
+//   const hash1 = new Set(nums1);
+//   const hash2 = new Set(nums2);
+//   const result = [];
+//
+//   for (let i of hash1) {
+//     if (hash2.has(i)) {
+//       const count1 = nums1.filter((item) => item === i).length;
+//       const count2 = nums2.filter((item) => item === i).length;
+//       let loop = Math.min(count2, count1);
+//
+//       while (loop > 0) {
+//         result.push(i);
+//         loop--;
+//       }
+//     }
+//   }
+//
+//   return result;
+// };
+
+// 2 - solution
+
 const intersectionOfTwoArray2 = (nums1, nums2) => {
-  const hash1 = new Set(nums1);
-  const hash2 = new Set(nums2);
-  const joinedHash = new Set([...Array.from(hash1), ...Array.from(hash2)]);
-  const joinedArr = Array.from(joinedHash);
   const result = [];
 
-  for (let i = 0; i < joinedArr.length; i++) {
-    const value = joinedArr[i];
-    if (hash1.has(value) && hash2.has(value)) {
-      const count1 = nums1.filter((item) => item === value).length;
-      const count2 = nums2.filter((item) => item === value).length;
-      let loopCount = Math.min(count1, count2);
+  for (let i = 0; i < nums1.length; i++) {
+    if (!hash.has(nums1[i])) {
+      const count = Math.min(
+        nums1.filter((item) => item === nums1[i]).length,
+        nums2.filter((item) => item === nums1[i]).length,
+      );
 
-      result.push(Array(loopCount).fill(value));
+      hash.set(nums1[i], count);
     }
   }
 
-  return result.reduce((prev, item) => [...prev, ...item], []);
+  return Array.from(hash.entries()).flatMap(([value, count]) =>
+    Array(count).fill(value),
+  );
 };
 
-const num1 = [4, 2, 2, 5];
-const num2 = [9, 4, 2, 2, 2, 8, 4];
+const num1 = [2, 2, 4, 5];
+const num2 = [2, 2, 2, 4, 4, 8, 9];
 
 console.log(intersectionOfTwoArray2(num1, num2));
