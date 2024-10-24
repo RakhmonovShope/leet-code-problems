c9e332bdb5cc36cec882b4be699fb366d789f36c
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-24 19:55:54 +0500

    feat(RansomNote): second solution also provided

--- js/leetcode/string/canConstruct.js
+++ js/leetcode/string/canConstruct.js
@@ -1,19 +1,38 @@
-const canConstruct = (ransomNote, magazine) => {
-  const hash = new Set();
+// 1 - solution
 
-  for (let i = 0; i < ransomNote.length; i++) {
-    for (let j = 0; j < magazine.length; j++) {
-      if (ransomNote[i] === magazine[j] && !hash.has(j)) {
-        hash.add(j);
-        break;
-      }
-    }
+// const canConstruct = (ransomNote, magazine) => {
+//   const hash = new Set();
+//
+//   for (let i = 0; i < ransomNote.length; i++) {
+//     for (let j = 0; j < magazine.length; j++) {
+//       if (ransomNote[i] === magazine[j] && !hash.has(j)) {
+//         hash.add(j);
+//         break;
+//       }
+//     }
+//   }
+
+//
+// return hash.size !== ransomNote.length;
+// };
+
+// 2 - solution
+
+const canConstruct = (ransomNote, magazine) => {
+  const rArr = ransomNote.split('');
+  const mArr = magazine.split('');
+
+  for (let i = 0; i < rArr.length; i++) {
+    if (
+      rArr.filter((item) => item === rArr[i]).length >
+      mArr.filter((item) => item === rArr[i]).length
+    )
+      return false;
   }
 
-  return hash.size === ransomNote.length;
+  return true;
 };
-
 const ransomNote = 'aa';
-const magazine = 'ab';
+const magazine = 'aab';
 
 console.log(canConstruct(ransomNote, magazine));
