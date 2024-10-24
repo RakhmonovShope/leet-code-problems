56a2b8cbca3ac9ab02d03be70d0d88a630a05ff2
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-24 20:03:44 +0500

    feat(firstUniqChar): second solution provided

--- js/leetcode/string/firstUniqueCharacterInString.js
+++ js/leetcode/string/firstUniqueCharacterInString.js
@@ -1,10 +1,27 @@
-var firstUniqChar = function (s) {
-  for (let i = 0; i < s.length; i++) {
-    const regex = new RegExp(s[i], 'g');
+// 1 - solution;
+// var firstUniqChar = function (s) {
+//   for (let i = 0; i < s.length; i++) {
+//     const regex = new RegExp(s[i], 'g');
+//
+//     const matches = s.match(regex);
+//
+//     if (matches.length === 1) return i;
+//   }
+//
+//   return -1;
+// };
 
-    const matches = s.match(regex);
+const firstUniqChar = (s) => {
+  const unique = new Set(s);
 
-    if (matches.length === 1) return i;
+  for (let i of unique) {
+    const copy = s;
+
+    const replacedCopy = copy.replaceAll(i, '');
+
+    if (s.length - replacedCopy.length === 1) {
+      return s.indexOf(i);
+    }
   }
 
   return -1;
