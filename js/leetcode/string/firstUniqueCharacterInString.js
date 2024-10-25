9286d7388a0d328b2c3ab1d76dd772111e8cdf4c
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-25 21:08:38 +0500

    feat(firstUniqChar): 5 - solution provided

--- js/leetcode/string/firstUniqueCharacterInString.js
+++ js/leetcode/string/firstUniqueCharacterInString.js
@@ -62,24 +62,40 @@
 //   return -1;
 // };
 
+// 5 - solution
+
 const firstUniqChar = (s) => {
   const map = new Map();
+  const queue = [];
 
   for (let i = 0; i < s.length; i++) {
     if (map.has(s[i])) {
       map.set(s[i], map.get(s[i]) + 1);
     } else {
       map.set(s[i], 1);
+      queue.push(s[i]);
+    }
+
+    if (map.get(queue[0]) > 1) {
+      queue.shift();
     }
   }
 
-  for (let j = 0; j < s.length; j++) {
-    if (map.get(s[j]) === 1) return j;
+  let letter;
+
+  for (let j = 0; j < queue.length; j++) {
+    if (map.get(queue[j]) === 1) {
+      letter = queue[j];
+      break;
+    } else {
+      queue.shift();
+    }
   }
 
-  return -1;
+  return s.indexOf(letter);
 };
 
-const s = 'loveleetcode';
+const s =
+  'itwqbtcdprfsuprkrjkausiterybzncbmdvkgljxuekizvaivszowqtmrttiihervpncztuoljftlxybpgwnjb';
 
 console.log('firstUniqChar', firstUniqChar(s));
