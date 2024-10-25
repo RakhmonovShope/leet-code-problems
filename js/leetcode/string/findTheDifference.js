21eb25315778330bdcc164db412990c2b799922b
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-25 22:49:13 +0500

    feat(findDifference): third solution provided

--- js/leetcode/string/findTheDifference.js
+++ js/leetcode/string/findTheDifference.js
@@ -8,40 +8,68 @@
 //   return t;
 // };
 
+// const findTheDifference = (s, t) => {
+//   const quickSort = (str) => {
+//     if (str.length < 2) return str;
+//
+//     let pivot = Math.floor(str.length / 2);
+//     const pivotValue = str[pivot];
+//
+//     let left = '';
+//     let right = '';
+//
+//     for (let i = 0; i < str.length; i++) {
+//       if (i !== pivot) {
+//         if (str[i] > pivotValue) {
+//           left += str[i];
+//         } else {
+//           right += str[i];
+//         }
+//       }
+//     }
+//
+//     return quickSort(left) + pivotValue + quickSort(right);
+//   };
+//
+//   const sortedT = quickSort(t);
+//   const sortedS = quickSort(s);
+//
+//   for (let i = 0; i < sortedT.length; i++) {
+//     if (sortedT[i] !== sortedS[i]) return sortedT[i];
+//
+//     if (i === sortedT.length - 1) return sortedT[i];
+//   }
+// };
+
 const findTheDifference = (s, t) => {
-  const quickSort = (str) => {
-    if (str.length < 2) return str;
+  const map = new Map();
 
-    let pivot = Math.floor(str.length / 2);
-    const pivotValue = str[pivot];
-
-    let left = '';
-    let right = '';
-
-    for (let i = 0; i < str.length; i++) {
-      if (i !== pivot) {
-        if (str[i] > pivotValue) {
-          left += str[i];
-        } else {
-          right += str[i];
-        }
-      }
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
     }
-
-    return quickSort(left) + pivotValue + quickSort(right);
-  };
-
-  const sortedT = quickSort(t);
-  const sortedS = quickSort(s);
-
-  for (let i = 0; i < sortedT.length; i++) {
-    if (sortedT[i] !== sortedS[i]) return sortedT[i];
-
-    if (i === sortedT.length - 1) return sortedT[i];
   }
+
+  for (let j = 0; j < t.length; j++) {
+    if (map.has(t[j])) {
+      map.set(t[j], map.get(t[j]) - 1);
+    } else {
+      map.set(t[j], 1);
+    }
+  }
+
+  let result;
+
+  map.forEach((value, key) => {
+    if (value === 1 || value === -1) result = key;
+  });
+
+  return result;
 };
 
-const s = 'sesware';
-const t = 'seswaret';
+const s = 'a';
+const t = 'aa';
 
 console.log(findTheDifference(s, t));
