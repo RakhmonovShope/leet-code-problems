ad088cfad4536d3343cfb288fc77ca409fc0f0a4
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-25 21:19:41 +0500

    feat(firstUniqChar): fixed 5- soution

--- js/leetcode/string/firstUniqueCharacterInString.js
+++ js/leetcode/string/firstUniqueCharacterInString.js
@@ -64,6 +64,35 @@
 
 // 5 - solution
 
+// const firstUniqChar = (s) => {
+//   const map = new Map();
+//   const queue = [];
+//
+//   for (let i = 0; i < s.length; i++) {
+//     if (map.has(s[i])) {
+//       map.set(s[i], map.get(s[i]) + 1);
+//     } else {
+//       map.set(s[i], 1);
+//       queue.push(s[i]);
+//     }
+//   }
+//
+//   let letter;
+//
+//   for (let j = 0; j < queue.length; j++) {
+//     if (map.get(queue[j]) === 1) {
+//       letter = queue[j];
+//       break;
+//     } else {
+//       queue.shift();
+//     }
+//   }
+//
+//   return s.indexOf(letter);
+// };
+
+// 6 - solution
+
 const firstUniqChar = (s) => {
   const map = new Map();
   const queue = [];
@@ -75,26 +104,16 @@ const firstUniqChar = (s) => {
       map.set(s[i], 1);
       queue.push(s[i]);
     }
-
-    if (map.get(queue[0]) > 1) {
-      queue.shift();
-    }
   }
 
-  let letter;
-
-  for (let j = 0; j < queue.length; j++) {
-    if (map.get(queue[j]) === 1) {
-      letter = queue[j];
-      break;
-    } else {
-      queue.shift();
-    }
+  while (queue.length && map.get(queue[0]) > 1) {
+    queue.shift();
   }
 
-  return s.indexOf(letter);
+  if (queue.length) {
+    return s.indexOf(queue[0]);
+  } else return -1;
 };
-
 const s =
   'itwqbtcdprfsuprkrjkausiterybzncbmdvkgljxuekizvaivszowqtmrttiihervpncztuoljftlxybpgwnjb';
 
