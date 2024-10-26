cc622b292c2ba3e28368848fc7b32efc9fd07caf
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-26 22:52:08 +0500

    feat: fizzBuzz problem solved

---
+++ js/leetcode/array/fizzBuzz.js
@@ -0,0 +1,21 @@
+var fizzBuzz = function (n) {
+  const arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(String(i));
+    }
+  }
+
+  return arr;
+};
+
+const n = 15;
+
+console.log(fizzBuzz(n));
