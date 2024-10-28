d31ed17a17e66f627c3b0f8026766a38fa16b16b
rahmonovshohboz <rakhmonovshope0110@mail.ru>
2024-10-28 22:17:18 +0500

    feat(longestPolindrom): 2 - solution provided

--- js/leetcode/string/longestPalindrom.js
+++ js/leetcode/string/longestPalindrom.js
@@ -1,8 +1,56 @@
+// 1 - solution
+// var longestPalindrome = function (s) {
+//   const hash = new Map();
+//
+//   let leftString = [];
+//   let rightString = [];
+//   let singleString = '';
+//
+//   for (let i = 0; i < s.length; i++) {
+//     if (hash.has(s[i])) {
+//       hash.set(s[i], hash.get(s[i]) + 1);
+//     } else {
+//       hash.set(s[i], 1);
+//     }
+//   }
+//
+//   for (let [key, value] of hash) {
+//     if (value === 1) {
+//       singleString += key;
+//       hash.delete(key);
+//     }
+//   }
+//
+//   for (let [key, value] of hash) {
+//     let count = 0;
+//
+//     if (value % 2 === 0) {
+//       count = value / 2;
+//     } else {
+//       count = Math.floor(value / 2);
+//       singleString += key;
+//     }
+//
+//     const stringArr = Array(count).fill(key);
+//
+//     leftString.push(...stringArr);
+//     rightString.push(...stringArr);
+//   }
+//
+//   if (singleString.length) {
+//     return `${leftString}${singleString[0]}${rightString}`.length;
+//   }
+//
+//   return `${leftString.join('')}${rightString}`.length;
+// };
+
+// 2 - solution
+
 var longestPalindrome = function (s) {
   const hash = new Map();
 
-  let leftString = [];
-  let rightString = [];
+  let leftString = '';
+  let rightString = '';
   let singleString = '';
 
   for (let i = 0; i < s.length; i++) {
@@ -21,6 +69,9 @@ var longestPalindrome = function (s) {
   }
 
   for (let [key, value] of hash) {
+    const lLength = leftString.length;
+    const rLength = rightString.length;
+
     let count = 0;
 
     if (value % 2 === 0) {
@@ -30,17 +81,15 @@ var longestPalindrome = function (s) {
       singleString += key;
     }
 
-    const stringArr = Array(count).fill(key);
-
-    leftString.push(...stringArr);
-    rightString.push(...stringArr);
+    leftString = leftString.padStart(count + lLength, key);
+    rightString = rightString.padEnd(count + rLength, key);
   }
 
   if (singleString.length) {
-    return `${leftString}${singleString[0]}${rightString}`;
+    return `${leftString + singleString[0] + rightString}`.length;
   }
 
-  return `${leftString.join('')}${rightString}`;
+  return `${leftString + rightString}`.length;
 };
 
 const s = 'abccccddttttt';
