8dd8b8e76f3090a3dccbc3fce548b77a41ad6c5b
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-10-31 23:37:35 +0500

    feat: reverse list problem solved

---
+++ js/leetcode/linkedList/reverseList.js
@@ -0,0 +1,12 @@
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
