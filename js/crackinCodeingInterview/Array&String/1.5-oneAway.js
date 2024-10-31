37c4dc5c5d1cba5e7aaeeb8f7b0fe5b33686d6b8
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-10-31 18:55:09 +0500

    feat(1.5): problem optimized and reduced redundant code into one single check

--- js/crackinCodeingInterview/Array&String/1.5-oneAway.js
+++ js/crackinCodeingInterview/Array&String/1.5-oneAway.js
@@ -1,63 +1,30 @@
-const oneAway = (str1, str2) => {
-  const removeCheck = (s1, s2) => {
-    let count = 0;
-    let j = 0;
+const oneAway = (s1, s2) => {
+  let count = 0;
+  let j = 0;
 
-    while (j < s1.length) {
-      if (s1[j] !== s2[j - count]) {
-        count++;
-      }
-
-      if (count > 1) return false;
-
-      j++;
-    }
-
-    return count === 1;
-  };
-
-  const insertCheck = (s1, s2) => {
-    let count = 0;
-    let j = 0;
-
-    while (j < s2.length) {
+  while (j < Math.max(s1.length, s2.length)) {
+    if (s1.length < s2.length) {
       if (s1[j - count] !== s2[j]) {
         count++;
       }
-
-      if (count > 1) return false;
-      j++;
-    }
-
-    return count === 1;
-  };
-
-  const checkInsertAndRemove = (s1, s2) => {
-    let count = 0;
-    let j = 0;
-  };
-
-  const replaceCheck = (s1, s2) => {
-    let count = 0;
-    for (let i = 0; i < s1.length; i++) {
-      if (s1[i] !== s2[i]) {
+    } else if (s1.length > s2.length) {
+      if (s1[j] !== s2[j - count]) {
         count++;
       }
-
-      if (count > 1) return false;
+    } else {
+      if (s1[j] !== s2[j]) {
+        count++;
+      }
     }
 
-    return count === 1;
-  };
+    if (count > 1) return false;
+    j++;
+  }
 
-  return (
-    removeCheck(str1, str2) ||
-    replaceCheck(str1, str2) ||
-    insertCheck(str1, str2)
-  );
+  return count === 1;
 };
 
 const str1 = 'pale';
-const str2 = 'pae';
+const str2 = 'bale';
 
 console.log(oneAway(str1, str2));
