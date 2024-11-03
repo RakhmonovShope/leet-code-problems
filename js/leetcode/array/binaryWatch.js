e49e53de016d40a316c33f9ad7766841783e2731
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-03 21:02:37 +0500

    feat: binary watch problem must be solved

---
+++ js/leetcode/array/binaryWatch.js
@@ -0,0 +1,13 @@
+var readBinaryWatch = function (turnedOn) {
+  const topLeds = [1, 2, 4, 8];
+  const bottomLeds = [1, 2, 4, 8, 16, 32];
+  let hours = '';
+  let minutes = '';
+
+
+
+};
+
+
+4 + 2 + 1 = 7
+16 + 8 + 4 + 2 + 1 = 31 + 32 = 55
