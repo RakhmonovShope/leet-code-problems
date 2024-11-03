6b1e53a6c0840702ce3432106dadb7402d16ba89
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-04 00:09:14 +0500

    feat: add numbers problem solved

---
+++ js/leetcode/math/addStrings.js
@@ -0,0 +1,10 @@
+const addStrings = (num1, num2) => {
+  let result = '';
+
+  for (let i = 0; i < Math.max(num1.length, num2.length); i++) {}
+};
+
+const num2 = '123';
+const num1 = '11';
+
+console.log(addStrings(num1, num2));
