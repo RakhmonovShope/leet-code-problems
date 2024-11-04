b2cba55fe133fcc8056568e5368f25b0a0900061
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-04 23:29:53 +0500

    feat(repeatedSubstringProblem): 2 - solution provided

--- js/leetcode/string/repeatedSubstringPattern.js
+++ js/leetcode/string/repeatedSubstringPattern.js
@@ -1,23 +1,29 @@
-const repeatedSubstringPattern = (s) => {
-  if (s.length <= 1) return false;
+// const repeatedSubstringPattern = (s) => {
+//   if (s.length <= 1) return false;
+//
+//   let newPattern = '';
+//
+//   for (let i = 0; i < s.length; i++) {
+//     const copy = newPattern.repeat(
+//       Math.floor(s.length / (newPattern.length || 1)),
+//     );
+//
+//     if (copy === s) {
+//       return true;
+//     } else {
+//       newPattern += s[i];
+//     }
+//   }
+//
+//   return false;
+// };
 
-  let newPattern = '';
-
-  for (let i = 0; i < s.length; i++) {
-    const copy = newPattern.repeat(
-      Math.floor(s.length / (newPattern.length || 1)),
-    );
-
-    if (copy === s) {
-      return true;
-    } else {
-      newPattern += s[i];
-    }
-  }
-
-  return false;
+const repeatedSubStringPatter = (s) => {
+  let double = s + s;
+  let sub = double.slice(1, -1);
+  return sub.includes(s);
 };
 
-const str = 'abaababaab';
+const str = 'abcabc';
 
-console.log(repeatedSubstringPattern(str));
+console.log(repeatedSubStringPatter(str));
