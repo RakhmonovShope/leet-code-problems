57f3e4c344b19ff2cef3aeb163bf1143c4a35bff
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-08 21:55:42 +0500

    feat(findAllNumbersDisappearedInArray): second solution provided

--- js/leetcode/array/findAllNumbersDisappearedInArray.js
+++ js/leetcode/array/findAllNumbersDisappearedInArray.js
@@ -1,22 +1,38 @@
+// const findAllNumbersDisappearedInArray = (nums) => {
+//   const arr = [];
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     const index = Math.abs(nums[i]) - 1;
+//
+//     if (nums[index] > 0) {
+//       nums[index] = -nums[index];
+//     }
+//   }
+//
+//   for (let j = 0; j < nums.length; j++) {
+//     if (nums[j] > 0) arr.push(j + 1);
+//   }
+//
+//   return arr;
+// };
+
 const findAllNumbersDisappearedInArray = (nums) => {
-  const arr = [];
+  let count = new Array(nums.length + 1).fill(0);
 
-  for (let i = 0; i < nums.length; i++) {
-    const index = Math.abs(nums[i]) - 1;
-
-    if (nums[index] > 0) {
-      nums[index] = -nums[index];
-    }
+  for (let i of nums) {
+    count[i]++;
   }
 
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] > 0) arr.push(j + 1);
+  const result = [];
+
+  for (let i = 1; i < count.length; i++) {
+    if (!count[i]) result.push(i);
   }
 
-  return arr;
+  return result;
 };
 
-const nums = [4, 3, 2, 7, 8, 2, 3, 1];
+const nums = [1, 1];
 // const nums = [2, 2];
 
 console.log(findAllNumbersDisappearedInArray(nums));
