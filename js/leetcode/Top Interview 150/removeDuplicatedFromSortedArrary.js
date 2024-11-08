32fb69ae4fbd83a185d8de5539f42f969e4c3597
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-09 01:47:26 +0500

    feat(removeDuplicatesFromSortedArray): second solution provided

---
+++ js/leetcode/Top Interview 150/removeDuplicatedFromSortedArrary.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var removeDuplicates = function (nums) {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[j] !== nums[i]) {
+      nums[j + 1] = nums[i];
+      j++;
+    }
+  }
+
+  for (let i = j + 1; i < nums.length; i++) {
+    nums[i] = undefined;
+  }
+
+  return nums.filter((item) => item !== undefined).length;
+};
+
+removeDuplicates([1, 1, 2]);
