d7928be703e298feddf9d22f652f07c3b55916e0
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-09 01:57:35 +0500

    feat(removeDuplicatesFromSortedArray): fourth solution provided

--- js/leetcode/Top Interview 150/removeDuplicatedFromSortedArrary.js
+++ js/leetcode/Top Interview 150/removeDuplicatedFromSortedArrary.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 
-// var removeDuplicates = function (nums) {
-//   let j = 0;
-//
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[j] !== nums[i]) {
-//       nums[j + 1] = nums[i];
-//       j++;
-//     }
-//   }
-//
-//   for (let i = j + 1; i < nums.length; i++) {
-//     nums[i] = undefined;
-//   }
-//
-//   return nums.filter((item) => item !== undefined).length;
-// };
+var removeDuplicates = function (nums) {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[j] !== nums[i]) {
+      nums[j + 1] = nums[i];
+      j++;
+    }
+  }
+
+  for (let i = j + 1; i < nums.length; i++) {
+    nums[i] = undefined;
+  }
+
+  return nums.filter((item) => item !== undefined).length;
+};
 
 var removeDuplicates = function (nums) {
   const hash = new Set(nums);
@@ -28,7 +28,7 @@ var removeDuplicates = function (nums) {
     nums[i] = uniqueArr[i];
   }
 
-  return nums.filter((item) => item !== undefined).length;
+  return hash.size;
 };
 
 removeDuplicates([1, 1, 2]);
