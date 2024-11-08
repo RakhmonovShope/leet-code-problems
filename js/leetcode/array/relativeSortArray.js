4d9857ccc013ebd2b9c95c32f177951b450f9619
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-08 21:46:04 +0500

    feat(relativeSortArray): second solution provided

--- js/leetcode/array/relativeSortArray.js
+++ js/leetcode/array/relativeSortArray.js
@@ -1,20 +1,56 @@
+// const relativeSortArray = (arr1, arr2) => {
+//   const hash = new Map();
+//   const result = [];
+//   const notRelativeItems = [];
+//
+//   let i = 0;
+//
+//   while (i < arr1.length) {
+//     hash.set(arr1[i], (hash.get(arr1[i]) || 0) + 1);
+//     i++;
+//   }
+//
+//   for (let i of arr2) {
+//     result.push(...Array(hash.get(i)).fill(i));
+//   }
+//
+//   for (let i of arr1) {
+//     if (!arr2.includes(i)) {
+//       notRelativeItems.push(i);
+//     }
+//   }
+//
+//   notRelativeItems.sort((a, b) => a - b);
+//   result.push(...notRelativeItems);
+//
+//   return result;
+// };
+
 const relativeSortArray = (arr1, arr2) => {
-  const result = [];
-  const notRelativeItems = [];
-
-  for (let i of arr2) {
-    const items = arr1.filter((item) => item === i);
-
-    result.push(...items);
-  }
+  let count = new Array(1001).fill(0);
 
   for (let i of arr1) {
-    if (!arr2.includes(i)) {
-      notRelativeItems.push(i);
+    count[i]++;
+  }
+
+  const result = [];
+  for (let i of arr2) {
+    for (let j = 0; j < count[i]; j++) {
+      result.push(i);
+    }
+    count[i] = 0;
+  }
+
+  for (let i = 0; i < 1001; i++) {
+    for (let j = 0; j < count[i]; j++) {
+      result.push(i);
     }
   }
 
-  notRelativeItems.sort((a, b) => a - b);
-
-  return [...result, ...notRelativeItems];
+  return result;
 };
+
+const arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
+const arr2 = [2, 1, 4, 3, 9, 6];
+
+console.log(relativeSortArray(arr1, arr2));
