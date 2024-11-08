80cc4a93ec1081adf82f05e10d8f6825c25d559d
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-09 01:51:45 +0500

    feat(removeDuplicatesFromSortedArray): third solution provided

--- js/leetcode/Top Interview 150/removeDuplicatedFromSortedArrary.js
+++ js/leetcode/Top Interview 150/removeDuplicatedFromSortedArrary.js
@@ -3,18 +3,29 @@
  * @return {number}
  */
 
+// var removeDuplicates = function (nums) {
+//   let j = 0;
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[j] !== nums[i]) {
+//       nums[j + 1] = nums[i];
+//       j++;
+//     }
+//   }
+//
+//   for (let i = j + 1; i < nums.length; i++) {
+//     nums[i] = undefined;
+//   }
+//
+//   return nums.filter((item) => item !== undefined).length;
+// };
+
 var removeDuplicates = function (nums) {
-  let j = 0;
+  const hash = new Set(nums);
+  const uniqueArr = [...hash];
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[j] !== nums[i]) {
-      nums[j + 1] = nums[i];
-      j++;
-    }
-  }
-
-  for (let i = j + 1; i < nums.length; i++) {
-    nums[i] = undefined;
+    nums[i] = uniqueArr[i];
   }
 
   return nums.filter((item) => item !== undefined).length;
