4a82003a37d7b8d13f3c22381b701f5dc13b32d8
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-10 19:01:39 +0500

    feat(removeDuplicatedFromSortedArra2)

--- js/leetcode/Top Interview 150/removeDuplicatedFromSortedArray2.js
+++ js/leetcode/Top Interview 150/removeDuplicatedFromSortedArray2.js
@@ -1,23 +1,39 @@
-var removeDuplicates2 = function (nums) {
-  const hash = new Map();
-  const copy = [];
-  for (let i = 0; i < nums.length; i++) {
-    hash.set(nums[i], (hash.get(nums[i]) || 0) + 1);
-  }
+// var removeDuplicates2 = function (nums) {
+//   const hash = new Map();
+//   const copy = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     hash.set(nums[i], (hash.get(nums[i]) || 0) + 1);
+//   }
+//
+//   for ([key, value] of hash) {
+//     if (value >= 2) {
+//       copy.push(key, key);
+//     } else {
+//       copy.push(key);
+//     }
+//   }
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     nums[i] = copy[i] !== undefined ? copy[i] : undefined;
+//   }
+//
+//   return nums.filter((item) => item !== undefined).length;
+// };
 
-  for ([key, value] of hash) {
-    if (value >= 2) {
-      copy.push(key, key);
+var removeDuplicates2 = function (nums) {
+  let count = 0;
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[j] !== nums[i] && count <= 2) {
+      nums[j + 1] = nums[i];
+      j++;
     } else {
-      copy.push(key);
+      count++;
     }
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    nums[i] = copy[i] !== undefined ? copy[i] : undefined;
-  }
-
-  return nums.filter((item) => item !== undefined).length;
+  console.log(nums);
 };
 
 console.log(removeDuplicates2([0, 1, 1, 1, 1, 2, 2, 2, 4]));
