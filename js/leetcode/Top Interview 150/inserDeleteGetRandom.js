4f76417de19add72d7cec0f1de8fa7f37035cacb
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-11 02:51:45 +0500

    feat: inset delete get random problem solved 2- solution provided

--- js/leetcode/Top Interview 150/inserDeleteGetRandom.js
+++ js/leetcode/Top Interview 150/inserDeleteGetRandom.js
@@ -1,53 +1,47 @@
 var RandomizedSet = function () {
-  this.hash = new Map();
-  this.arr = [];
+  this.hash = new Set();
 };
 
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
 RandomizedSet.prototype.insert = function (val) {
   if (this.hash.has(val)) {
     return false;
+  } else {
+    this.hash.add(val);
+    return true;
   }
-
-  this.hash.set(val, this.arr.length);
-  this.arr.push(val);
-
-  return true;
 };
 
+/**
+ * @param {number} val
+ * @return {boolean}
+ */
 RandomizedSet.prototype.remove = function (val) {
-  if (!this.hash.has(val)) {
+  if (this.hash.has(val)) {
+    this.hash.delete(val);
+    return true;
+  } else {
     return false;
   }
-
-  const index = this.hash.get(val);
-
-  const temp = this.arr[this.arr.length - 1];
-
-  this.hash.set(temp, index);
-
-  this.arr[this.arr.length - 1] = this.arr[index];
-  this.arr[index] = temp;
-
-  this.arr.pop();
-  this.hash.delete(val);
-  return true;
 };
 
+/**
+ * @return {number}
+ */
 RandomizedSet.prototype.getRandom = function () {
-  const randomIndex = Math.floor(Math.random() * this.arr.length);
+  const setArr = [...this.hash];
+  const randomIndex = Math.floor(Math.random() * setArr.length);
 
-  return this.arr[randomIndex];
+  return setArr[randomIndex];
 };
 
-const val = 2;
-
-var obj = new RandomizedSet();
-console.log(obj.getRandom());
-console.log(obj.insert(1));
-console.log(obj.remove(2));
-console.log(obj.insert(2));
-console.log(obj.getRandom());
-console.log(obj.remove(1));
-console.log(obj);
-console.log(obj.insert(2));
-console.log(obj.getRandom());
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
