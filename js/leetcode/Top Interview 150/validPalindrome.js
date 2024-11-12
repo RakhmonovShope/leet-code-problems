6d19db28c0675abd1244c6670849ee46ff138b70
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-12 19:23:46 +0500

    feat(validPalindrome): third solution provided

--- js/leetcode/Top Interview 150/validPalindrome.js
+++ js/leetcode/Top Interview 150/validPalindrome.js
@@ -1,25 +1,41 @@
+// const validPalindrome = (s) => {
+//   let sLower = s.toLowerCase();
+//
+//   let copy = '';
+//   for (let i = 0; i < sLower.length; i++) {
+//     if (
+//       (48 <= sLower.charCodeAt(i) && sLower.charCodeAt(i) <= 57) ||
+//       (97 <= sLower.charCodeAt(i) && sLower.charCodeAt(i) <= 122)
+//     ) {
+//       copy += sLower[i];
+//     }
+//   }
+//
+//   let reversedCopy = '';
+//
+//   for (let i = copy.length - 1; i >= 0; i--) {
+//     reversedCopy += copy[i];
+//   }
+//
+//   return reversedCopy === copy;
+// };
+
 const validPalindrome = (s) => {
-  let sLower = s.toLowerCase();
+  let sLower = s.replace(/[^a-zA-Z0-9]/g, '');
+  sLower = sLower.toLowerCase();
+  let i = 0;
+  let j = sLower.length - 1;
 
-  let copy = '';
-  for (let i = 0; i < sLower.length; i++) {
-    if (
-      (48 <= sLower.charCodeAt(i) && sLower.charCodeAt(i) <= 57) ||
-      (97 <= sLower.charCodeAt(i) && sLower.charCodeAt(i) <= 122)
-    ) {
-      copy += sLower[i];
-    }
+  while (i !== Math.floor(sLower.length / 2)) {
+    if (sLower[i] !== sLower[j]) return false;
+
+    i++;
+    j--;
   }
 
-  let reversedCopy = '';
-
-  for (let i = copy.length - 1; i >= 0; i--) {
-    reversedCopy += copy[i];
-  }
-
-  return reversedCopy === copy;
+  return true;
 };
 
-const s = 'A man, a plan, a canal: Panama';
+const s = 'race a car';
 
 console.log(validPalindrome(s));
