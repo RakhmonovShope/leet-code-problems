1260ffc0879e8e38d0fd80e26eea6fe590656865
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-12 16:49:42 +0500

    feat(reverseWordsInString): second solution provided

--- js/leetcode/Top Interview 150/reverseWordsInString.js
+++ js/leetcode/Top Interview 150/reverseWordsInString.js
@@ -1,56 +1,76 @@
+// const reverseWordsInString = (s) => {
+//   const trim = (s) => {
+//     let prefixSpaceCount = 0;
+//     let suffixSpaceCount = 0;
+//     let i = 0;
+//
+//     while (i < s.length) {
+//       if (s[i] === ' ') {
+//         prefixSpaceCount++;
+//         i++;
+//       } else break;
+//     }
+//
+//     i = s.length - 1;
+//
+//     while (i >= prefixSpaceCount - 1) {
+//       if (s[i] === ' ') {
+//         suffixSpaceCount++;
+//         i--;
+//       } else break;
+//     }
+//
+//     i = prefixSpaceCount;
+//
+//     let result = '';
+//
+//     while (i <= s.length - 1 - suffixSpaceCount) {
+//       result += s[i];
+//       i++;
+//     }
+//
+//     return result;
+//   };
+//
+//   let trimmedString = trim(s);
+//
+//   let result = '';
+//   let copy = '';
+//
+//   for (let i = 0; i < trimmedString.length; i++) {
+//     if (trimmedString[i] !== ' ') {
+//       copy += trimmedString[i];
+//     } else {
+//       if (copy) {
+//         result = result ? `${copy} ` + result : copy + result;
+//         copy = '';
+//       }
+//     }
+//
+//     if (i === trimmedString.length - 1) {
+//       if (copy) {
+//         result = result ? `${copy} ` + result : copy + result;
+//       }
+//     }
+//   }
+//
+//   return result;
+// };
+
 const reverseWordsInString = (s) => {
-  const trim = (s) => {
-    let prefixSpaceCount = 0;
-    let suffixSpaceCount = 0;
-    let i = 0;
-
-    while (i < s.length) {
-      if (s[i] === ' ') {
-        prefixSpaceCount++;
-        i++;
-      } else break;
-    }
-
-    i = s.length - 1;
-
-    while (i >= prefixSpaceCount - 1) {
-      if (s[i] === ' ') {
-        suffixSpaceCount++;
-        i--;
-      } else break;
-    }
-
-    i = prefixSpaceCount;
-
-    let result = '';
-
-    while (i <= s.length - 1 - suffixSpaceCount) {
-      result += s[i];
-      i++;
-    }
-
-    return result;
-  };
-
-  let trimmedString = trim(s);
-
-  let result = '';
   let copy = '';
+  let result = '';
 
-  for (let i = 0; i < trimmedString.length; i++) {
-    if (trimmedString[i] !== ' ') {
-      copy += trimmedString[i];
-    } else {
-      if (copy) {
-        result = result ? `${copy} ` + result : copy + result;
-        copy = '';
-      }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== ' ') {
+      copy += s[i];
+    } else if (copy) {
+      result = result ? `${copy} ` + result : copy + result;
+      copy = '';
     }
 
-    if (i === trimmedString.length - 1) {
-      if (copy) {
-        result = result ? `${copy} ` + result : copy + result;
-      }
+    if (i === s.length - 1 && copy) {
+      result = result ? `${copy} ` + result : copy + result;
     }
   }
 
