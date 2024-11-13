fc928beb15428a331e60027ee18101da7df816c7
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-13 14:37:11 +0500

    feat(twoSum2): 4 solution provided

--- js/leetcode/Top Interview 150/twoSum2.js
+++ js/leetcode/Top Interview 150/twoSum2.js
@@ -18,35 +18,52 @@
 //   return [];
 // };
 
+// const twoSum2 = (numbers, target) => {
+//   const binarySearch = (nums, tar, excl) => {
+//     let max = nums.length - 1;
+//     let min = 0;
+//
+//     while (min <= max) {
+//       let mid = Math.floor((max + min) / 2);
+//
+//       if (nums[mid] === tar && mid !== excl) return mid;
+//       if (nums[mid] > tar) {
+//         max = mid - 1;
+//       } else {
+//         min = mid + 1;
+//       }
+//     }
+//
+//     return -1;
+//   };
+//
+//   for (let i = 0; i < numbers.length; i++) {
+//     const indexOfNumber = binarySearch(numbers, target - numbers[i], i);
+//
+//     if (indexOfNumber !== -1) return [i + 1, indexOfNumber + 1];
+//   }
+//
+//   return [];
+// };
+
 const twoSum2 = (numbers, target) => {
-  const binarySearch = (nums, tar, excl) => {
-    let max = nums.length - 1;
-    let min = 0;
+  let j = 0;
+  let i = numbers.length - 1;
 
-    while (min <= max) {
-      let mid = Math.floor((max + min) / 2);
-
-      if (nums[mid] === tar && mid !== excl) return mid;
-      if (nums[mid] > tar) {
-        max = mid - 1;
-      } else {
-        min = mid + 1;
-      }
+  while (j < i) {
+    if (numbers[j] + numbers[i] === target) {
+      return [j + 1, i + 1];
+    } else if (numbers[j] + numbers[i] < target) {
+      j++;
+    } else {
+      i--;
     }
-
-    return -1;
-  };
-
-  for (let i = 0; i < numbers.length; i++) {
-    const indexOfNumber = binarySearch(numbers, target - numbers[i], i);
-
-    if (indexOfNumber !== -1) return [i + 1, indexOfNumber + 1];
   }
 
   return [];
 };
 
-const target = 8;
-const numbers = [1, 2, 3, 4, 4, 9, 56, 90];
+const target = 100;
+const numbers = [5, 25, 75];
 
 console.log(twoSum2(numbers, target));
