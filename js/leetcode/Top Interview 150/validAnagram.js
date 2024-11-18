0b3be02383bc73fffb2acbf465a3df8bfd5ef4da
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-18 21:33:53 +0500

    feat(validAnagram): second solution provided

---
+++ js/leetcode/Top Interview 150/validAnagram.js
@@ -0,0 +1,23 @@
+const validAnagram = (s, t) => {
+  const hash = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    hash.set(s[i], (hash.get(s[i]) || 0) + 1);
+  }
+
+  for (let j = 0; j < t.length; j++) {
+    if (hash.has(t[j])) {
+      hash.set(t[j], hash.get(t[j]) - 1);
+    } else {
+      hash.set(t[j], hash.get((t[j] || 0) + 1));
+    }
+  }
+
+  for (let [key, value] of hash) {
+    if (value !== 0) return false;
+  }
+
+  return true;
+};
+
+console.log(validAnagram('anagram', 'nagaram'));
