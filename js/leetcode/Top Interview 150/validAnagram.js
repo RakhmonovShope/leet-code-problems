651f511f225e9d903a626c42c386f9526621dcc7
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-18 22:43:04 +0500

    feat(validAnagram): fourth solution provided

--- js/leetcode/Top Interview 150/validAnagram.js
+++ js/leetcode/Top Interview 150/validAnagram.js
@@ -32,34 +32,50 @@
 //   return true;
 // };
 
-const quickSort = (str) => {
-  if (str.length < 2) return str;
-
-  let pivot = Math.floor(str.length / 2);
-  const pivotValue = str[pivot];
-  let left = '';
-  let right = '';
-
-  for (let i = 0; i < str.length; i++) {
-    if (i !== pivot) {
-      if (str.charCodeAt(i) > str.charCodeAt(pivot)) {
-        right += str[i];
-      } else {
-        left += str[i];
-      }
-    }
-  }
-
-  return quickSort(left) + pivotValue + quickSort(right);
-};
+// const quickSort = (str) => {
+//   if (str.length < 2) return str;
+//
+//   let pivot = Math.floor(str.length / 2);
+//   const pivotValue = str[pivot];
+//   let left = '';
+//   let right = '';
+//
+//   for (let i = 0; i < str.length; i++) {
+//     if (i !== pivot) {
+//       if (str.charCodeAt(i) > str.charCodeAt(pivot)) {
+//         right += str[i];
+//       } else {
+//         left += str[i];
+//       }
+//     }
+//   }
+//
+//   return quickSort(left) + pivotValue + quickSort(right);
+// };
+//
+// const validAnagram = (s, t) => {
+//   if (s.length !== t.length) return false;
+//
+//   const sortedS = quickSort(s);
+//   const sortedT = quickSort(t);
+//
+//   return sortedS === sortedT;
+// };
 
 const validAnagram = (s, t) => {
   if (s.length !== t.length) return false;
+  const hash = new Map();
 
-  const sortedS = quickSort(s);
-  const sortedT = quickSort(t);
+  for (let i = 0; i < s.length; i++) {
+    hash.set(s[i], (hash.get(s[i]) || 0) + 1);
+    hash.set(t[i], (hash.get(t[i]) || 0) - 1);
+  }
 
-  return sortedS === sortedT;
+  for (let [key, value] of hash) {
+    if (value !== 0) return false;
+  }
+
+  return true;
 };
 
-console.log(validAnagram('anagrams', 'nagaradm'));
+console.log(validAnagram('anagram', 'nagaram'));
