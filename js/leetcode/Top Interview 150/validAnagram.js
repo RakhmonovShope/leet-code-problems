a2549efe913b1f609be64443a88123b20121a611
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-18 21:41:17 +0500

    feat(validAnagram): second solution provided

--- js/leetcode/Top Interview 150/validAnagram.js
+++ js/leetcode/Top Interview 150/validAnagram.js
@@ -1,23 +1,35 @@
+// const validAnagram = (s, t) => {
+//   const hash = new Map();
+//
+//   for (let i = 0; i < s.length; i++) {
+//     hash.set(s[i], (hash.get(s[i]) || 0) + 1);
+//   }
+//
+//   for (let j = 0; j < t.length; j++) {
+//     if (hash.has(t[j])) {
+//       hash.set(t[j], hash.get(t[j]) - 1);
+//     } else {
+//       hash.set(t[j], hash.get((t[j] || 0) + 1));
+//     }
+//   }
+//
+//   for (let [key, value] of hash) {
+//     if (value !== 0) return false;
+//   }
+//
+//   return true;
+// };
+
 const validAnagram = (s, t) => {
-  const hash = new Map();
+  if (s.length !== t.length) return false;
 
   for (let i = 0; i < s.length; i++) {
-    hash.set(s[i], (hash.get(s[i]) || 0) + 1);
+    t = t.replace(s[i], '');
   }
 
-  for (let j = 0; j < t.length; j++) {
-    if (hash.has(t[j])) {
-      hash.set(t[j], hash.get(t[j]) - 1);
-    } else {
-      hash.set(t[j], hash.get((t[j] || 0) + 1));
-    }
-  }
-
-  for (let [key, value] of hash) {
-    if (value !== 0) return false;
-  }
+  if (t.length) return false;
 
   return true;
 };
 
-console.log(validAnagram('anagram', 'nagaram'));
+console.log(validAnagram('anagrdam', 'nagarsam'));
