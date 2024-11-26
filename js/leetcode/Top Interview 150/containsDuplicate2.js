e6857bbb7e15643ff65d7435039b82201e4360db
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-11-26 20:02:56 +0500

    feat(ContainsDuplicate2): second solution provided

--- js/leetcode/Top Interview 150/containsDuplicate2.js
+++ js/leetcode/Top Interview 150/containsDuplicate2.js
@@ -1,30 +1,49 @@
+// const containsDuplicate2 = (nums, k) => {
+//   const hash = new Map();
+//
+//   for (let i = 0; i < nums.length; i++) {
+//     if (hash.has(nums[i])) {
+//       hash.set(nums[i], [...hash.get(nums[i]), i]);
+//     } else {
+//       hash.set(nums[i], [i]);
+//     }
+//   }
+//
+//   for (let [key, arr] of hash) {
+//     if (arr.length > 1) {
+//       let subtract = -1;
+//
+//       for (let j = 0; j < arr.length; j++) {
+//         subtract = Math.abs(arr[j] - arr[j + 1]);
+//
+//         if (subtract <= k) return true;
+//       }
+//     }
+//   }
+//
+//   return false;
+// };
+
 const containsDuplicate2 = (nums, k) => {
-  const hash = new Map();
+  const s = new Set();
 
+  // Traverse the input array
   for (let i = 0; i < nums.length; i++) {
-    if (hash.has(nums[i])) {
-      hash.set(nums[i], [...hash.get(nums[i]), i]);
-    } else {
-      hash.set(nums[i], [i]);
-    }
+    // If already present in hash, then we found
+    // a duplicate within k distance
+
+    if (s.has(nums[i])) return true;
+
+    // Add this item to hashset
+    s.add(nums[i]);
+
+    // Remove the k+1 distant item
+    if (i >= k) s.delete(nums[i - k]);
   }
-
-  for (let [key, arr] of hash) {
-    if (arr.length > 1) {
-      let subtract = -1;
-
-      for (let j = 0; j < arr.length; j++) {
-        subtract = Math.abs(arr[j] - arr[j + 1]);
-
-        if (subtract <= k) return true;
-      }
-    }
-  }
-
   return false;
 };
 
-const nums = [1, 2, 3, 1, 2, 3];
+const nums = [1, 2, 3, 1];
 const k = 2;
 
 console.log(containsDuplicate2(nums, k));
