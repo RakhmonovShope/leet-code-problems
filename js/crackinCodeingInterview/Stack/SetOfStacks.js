5b3b426f7ccc15c18bc69fa5f83e1dde736e6a72
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-12-07 23:08:04 +0500

    feat(Stack): set of stack problem solved but not complete

---
+++ js/crackinCodeingInterview/Stack/SetOfStacks.js
@@ -0,0 +1,33 @@
+class SetOfStacks {
+  constructor(props) {
+    this.items = [];
+    this.threeshold = 3;
+  }
+
+  push(value) {
+    const top = this.items[this.items.length - 1] || [];
+
+    if (top.length !== this.threeshold) {
+      top.push(value);
+      this.items[this.items.length - 1] = top;
+    } else {
+      this.items.push([value]);
+    }
+  }
+
+  pop() {
+    if (this.items[this.items.length - 1].length === 1) {
+      this.items = this.items.splice(this.items.length - 1);
+    } else {
+      this.items[this.items.length - 1].pop();
+    }
+  }
+}
+
+const stack = new SetOfStacks();
+
+stack.push(23);
+stack.push(11);
+stack.push(145);
+stack.push(2);
+console.log(stack);
