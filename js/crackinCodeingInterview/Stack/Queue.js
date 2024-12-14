f382728be98f8450d4fb02f85085d5c6f36d53ad
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-12-14 07:37:16 +0500

    feat(Queue): creating queue with linked list logic added

--- js/crackinCodeingInterview/Stack/Queue.js
+++ js/crackinCodeingInterview/Stack/Queue.js
@@ -28,38 +28,88 @@
 //   }
 // }
 
-class QueueWithObject {
+// class QueueWithObject {
+//   constructor() {
+//     this.items = {};
+//     this.front = 0;
+//     this.rear = 0;
+//   }
+//
+//   isEmpty() {
+//     return !Object.values(this.items).length;
+//   }
+//
+//   enqueue(value) {
+//     this.items[this.rear] = value;
+//     this.rear++;
+//   }
+//
+//   dequeue() {
+//     if (this.isEmpty()) return 'Queue is empty';
+//
+//     delete this.items[this.front];
+//
+//     this.front++;
+//   }
+//
+//   peek() {
+//     if (this.isEmpty()) return 'Queue is empty';
+//
+//     return this.items[this.front];
+//   }
+// }
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class QueueWithLinkedList {
   constructor() {
-    this.items = {};
-    this.front = 0;
-    this.rear = 0;
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
   }
 
   isEmpty() {
-    return !Object.values(this.items).length;
-  }
-
-  enqueue(value) {
-    this.items[this.rear] = value;
-    this.rear++;
-  }
-
-  dequeue() {
-    if (this.isEmpty()) return 'Queue is empty';
-
-    delete this.items[this.front];
-
-    this.front++;
+    return !this.size;
   }
 
   peek() {
     if (this.isEmpty()) return 'Queue is empty';
 
-    return this.items[this.front];
+    const head = this.front;
+
+    return head.value;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.front) {
+      this.front = newNode;
+    } else {
+      this.rear.next = newNode;
+    }
+
+    this.rear = newNode;
+    this.size++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return 'Queue is empty';
+    this.front = this.front.next;
+
+    if (!this.front) {
+      this.rear = null;
+    }
+    this.size--;
   }
 }
 
-const queueWithObject = new QueueWithObject();
+const queueWithObject = new QueueWithLinkedList();
 
 queueWithObject.enqueue(34);
 queueWithObject.enqueue(22);
