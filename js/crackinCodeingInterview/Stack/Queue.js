b24b244aeba952fa25f16a9439c5f79265324860
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2024-12-14 07:23:27 +0500

    feat(Queue): creating queue with object logic added

--- js/crackinCodeingInterview/Stack/Queue.js
+++ js/crackinCodeingInterview/Stack/Queue.js
@@ -1,29 +1,74 @@
-class Queue {
-  constructor() {
-    this.items = [];
-  }
+// class Queue {
+//   constructor() {
+//     this.items = [];
+//   }
+//
+//   enqueue(value) {
+//     this.items.push(value);
+//   }
+//
+//   isEmpty() {
+//     return !this.items.length;
+//   }
+//
+//   peek() {
+//     if (this.isEmpty()) {
+//       return 'Queue is empty';
+//     }
+//
+//     return this.items[0];
+//   }
+//
+//   dequeue() {
+//     if (this.isEmpty()) {
+//       return 'Queue is empty';
+//     }
+//
+//     return this.items.shift();
+//   }
+// }
 
-  enqueue(value) {
-    this.items.push(value);
+class QueueWithObject {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
   }
 
   isEmpty() {
-    return !this.items.length;
+    return !Object.values(this.items).length;
   }
 
-  peek() {
-    if (this.isEmpty()) {
-      return 'Queue is empty';
-    }
-
-    return this.items[0];
+  enqueue(value) {
+    this.items[this.rear] = value;
+    this.rear++;
   }
 
   dequeue() {
-    if (this.isEmpty()) {
-      return 'Queue is empty';
-    }
+    if (this.isEmpty()) return 'Queue is empty';
 
-    return this.items.shift();
+    delete this.items[this.front];
+
+    this.front++;
+  }
+
+  peek() {
+    if (this.isEmpty()) return 'Queue is empty';
+
+    return this.items[this.front];
   }
 }
+
+const queueWithObject = new QueueWithObject();
+
+queueWithObject.enqueue(34);
+queueWithObject.enqueue(22);
+console.log(queueWithObject);
+queueWithObject.dequeue();
+console.log(queueWithObject);
+queueWithObject.enqueue(13);
+console.log(queueWithObject);
+queueWithObject.dequeue();
+queueWithObject.dequeue();
+queueWithObject.enqueue(87);
+console.log(queueWithObject);
