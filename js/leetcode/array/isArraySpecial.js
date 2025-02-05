25dc46efca614ad4eb6b978b78bf5de306b68119
Rahmonov Shohboz <rakhmonovshope0110@mail.ru>
2025-02-05 17:20:26 +0500

    feat: is array special problem solved

---
+++ js/leetcode/array/isArraySpecial.js
@@ -0,0 +1,17 @@
+var isArraySpecial = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (
+      (nums[i] % 2 === 0 && nums[i + 1] % 2 === 0) ||
+      (nums[i] % 2 === 1 && nums[i + 1] % 2 === 1)
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// const arr = [2, 1, 4];
+const arr = [1];
+
+console.log(isArraySpecial(arr));
